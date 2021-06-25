2aa8d5bedbb1372fbf8ec68f8474395ef8adc4d6
liuran001 <32791471+liuran001@users.noreply.github.com>
2021-06-25 20:00:03 +0800

    Update packages-19.07.sh

--- packages-19.07.sh
+++ packages-19.07.sh
@@ -18,13 +18,7 @@ svn co https://github.com/jerrykuku/luci-app-vssr/trunk ./luci-app-vssr
 svn co https://github.com/tty228/luci-app-serverchan/trunk ./luci-app-serverchan
 svn co https://github.com/fw876/helloworld/trunk ./
 rm -rf .svn
-svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol ./
-rm -rf .svn
-svn co https://github.com/xiaorouji/openwrt-package/trunk/obsolete ./
-rm -rf .svn
-svn co https://github.com/xiaorouji/openwrt-package/trunk/others ./
-rm -rf .svn
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package ./
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk ./
 rm -rf .svn
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
 svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns
