0b816c27a1ccff44b9ee1e4c6a93329053c66372
liuran001 <32791471+liuran001@users.noreply.github.com>
2021-06-29 00:32:15 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -94,6 +94,7 @@ git clone https://github.com/jerrykuku/luci-app-argon-config
 svn co https://github.com/BCYDTZ/luci-app-UUGameAcc/trunk ./luci-app-UUGameAcc
 svn co https://github.com/garypang13/openwrt-bypass/trunk/luci-app-bypass
 svn co https://github.com/garypang13/openwrt-bypass/trunk/smartdns-le
+svn co https://github.com/esirplayground/luci-app-LingTiGameAcc/trunk
 
 rm -rf .svn
 rm -rf ./*/.git
