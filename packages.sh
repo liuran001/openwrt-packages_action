2c91392eb11754a7eaefc1a6e53c55f60b492f46
liuran001 <32791471+liuran001@users.noreply.github.com>
2021-06-29 14:40:11 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -38,7 +38,6 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-p
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-airplay2
 svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom
 svn co https://github.com/immortalwrt/luci-app-koolproxyR/trunk ./luci-app-koolproxyR
-svn co https://github.com/MiRouter/luci-app-vssr-plus/trunk ./luci-app-vssr-plus
 svn co https://github.com/immortalwrt/packages/trunk/net/GoQuiet
 svn co https://github.com/immortalwrt/packages/trunk/net/chinadns
 svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
